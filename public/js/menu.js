7bd48ab9bf51e7e558f6303569c21da8dd3cbea8
Wodnov <leo.hvs@gmail.com>
2018-11-19 20:53:25 +0100

    jason också

---
+++ public/js/menu.js
@@ -0,0 +1,33 @@
+var food = [
+    {
+        "name": "Dödsburgaren",
+        "cal": 2000,
+        "lactose": true,
+        "gluten": true,    
+        "img": "https://www.max.se/ImageVaultFiles/id_706/cf_22/Burgers-Maxburger.jpg"
+    },{
+        "name": "DABARGER!",
+        "cal": 69,
+        "lactose": true,
+        "gluten": false,    
+        "img": "https://www.max.se/ImageVaultFiles/id_702/cf_22/Burgers-Beefburger.jpg"
+    },{
+        "name": "Börgsson",
+        "cal": 13,
+        "lactose": false,
+        "gluten": true,    
+        "img": "https://www.max.se/ImageVaultFiles/id_720/cf_22/Meal-Delifresh-Low-Carb.jpg"
+    },{
+        "name": "Le Crunch",
+        "cal": 1337,
+        "lactose": true,
+        "gluten": true,    
+        "img": "https://www.max.se/ImageVaultFiles/id_4108/cf_22/Burgers-GDL-Crunchy-Nacho-Beef.jpg"
+    },{
+        "name": "XxX",
+        "cal": 666,
+        "lactose": true,
+        "gluten": true,    
+        "img": "https://www.max.se/ImageVaultFiles/id_718/cf_22/Burgers-GDL-Tripple-Cheese.jpg"
+    }
+]
